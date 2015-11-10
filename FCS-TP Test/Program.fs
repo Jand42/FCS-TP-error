3048b611351d1efa997f71676fa08076c1a542a3
András Jankó <andras.janko@gmail.com>
2015-11-10 15:14:03 +0100

    Failing test project

---
+++ FCS-TP Test/Program.fs
@@ -0,0 +1,127 @@
+﻿open System
+open System.IO
+open System.Collections.Generic
+open Microsoft.FSharp.Compiler
+open Microsoft.FSharp.Compiler.SourceCodeServices
+
+// Create an interactive checker instance 
+let checker = FSharpChecker.Create(keepAssemblyContents=true)
+
+[<AutoOpen>]
+module Utils = 
+    let rec printExpr low (e:FSharpExpr) = 
+        match e with 
+        | BasicPatterns.AddressOf(e1) -> "&"+printExpr 0 e1
+        | BasicPatterns.AddressSet(e1,e2) -> printExpr 0 e1 + " <- " + printExpr 0 e2
+        | BasicPatterns.Application(f,tyargs,args) -> quote low (printExpr 10 f + printTyargs tyargs + " " + printCurriedArgs args)
+        | BasicPatterns.BaseValue(_) -> "base"
+        | BasicPatterns.Call(Some obj,v,tyargs1,tyargs2,argsL) -> printObjOpt (Some obj) + v.CompiledName  + printTyargs tyargs2 + printTupledArgs argsL
+        | BasicPatterns.Call(None,v,tyargs1,tyargs2,argsL) -> v.EnclosingEntity.CompiledName + printTyargs tyargs1 + "." + v.CompiledName  + printTyargs tyargs2 + " " + printTupledArgs argsL
+        | BasicPatterns.Coerce(ty1,e1) -> quote low (printExpr 10 e1 + " :> " + printTy ty1)
+        | BasicPatterns.DefaultValue(ty1) -> "dflt"
+        | BasicPatterns.FastIntegerForLoop _ -> "for-loop"
+        | BasicPatterns.ILAsm(s,tyargs,args) -> s + printTupledArgs args 
+        | BasicPatterns.ILFieldGet _ -> "ILFieldGet"
+        | BasicPatterns.ILFieldSet _ -> "ILFieldSet"
+        | BasicPatterns.IfThenElse (a,b,c) -> "(if " + printExpr 0 a + " then " + printExpr 0 b + " else " + printExpr 0 c + ")"
+        | BasicPatterns.Lambda(v,e1) -> "fun " + v.CompiledName + " -> " + printExpr 0 e1
+        | BasicPatterns.Let((v,e1),b) -> "let " + (if v.IsMutable then "mutable " else "") + v.CompiledName + ": " + printTy v.FullType + " = " + printExpr 0 e1 + " in " + printExpr 0 b
+        | BasicPatterns.LetRec(vse,b) -> "let rec ... in " + printExpr 0 b
+        | BasicPatterns.NewArray(ty,es) -> "[| ... |]" 
+        | BasicPatterns.NewDelegate(ty,es) -> "new-delegate" 
+        | BasicPatterns.NewObject(v,tys,args) -> "new " + v.EnclosingEntity.CompiledName + printTupledArgs args 
+        | BasicPatterns.NewRecord(v,args) -> "{ ... }" 
+        | BasicPatterns.NewTuple(v,args) -> printTupledArgs args 
+        | BasicPatterns.NewUnionCase(ty,uc,args) -> uc.CompiledName + printTupledArgs args 
+        | BasicPatterns.Quote(e1) -> "quote" + printTupledArgs [e1]
+        | BasicPatterns.FSharpFieldGet(obj, ty,f) -> printObjOpt obj + f.Name 
+        | BasicPatterns.FSharpFieldSet(obj, ty,f,arg) -> printObjOpt obj + f.Name + " <- " + printExpr 0 arg
+        | BasicPatterns.Sequential(e1,e2) -> "(" + printExpr 0 e1 + "; " + printExpr 0 e2 + ")"
+        | BasicPatterns.ThisValue _ -> "this"
+        | BasicPatterns.TryFinally(e1,e2) -> "try " + printExpr 0 e1 + " finally " + printExpr 0 e2
+        | BasicPatterns.TryWith(e1,_,_,vC,eC) -> "try " + printExpr 0 e1 + " with " + vC.CompiledName + " -> " + printExpr 0 eC
+        | BasicPatterns.TupleGet(ty,n,e1) -> printExpr 10 e1 + ".Item" + string n
+        | BasicPatterns.DecisionTree(dtree,targets) -> "match " + printExpr 10 dtree + " targets ..."
+        | BasicPatterns.DecisionTreeSuccess (tg,es) -> "$" + string tg
+        | BasicPatterns.TypeLambda(gp1,e1) -> "FUN ... -> " + printExpr 0 e1 
+        | BasicPatterns.TypeTest(ty,e1) -> printExpr 10 e1 + " :? " + printTy ty
+        | BasicPatterns.UnionCaseSet(obj,ty,uc,f1,e1) -> printExpr 10 obj + "." + f1.Name + " <- " + printExpr 0 e1
+        | BasicPatterns.UnionCaseGet(obj,ty,uc,f1) -> printExpr 10 obj + "." + f1.Name
+        | BasicPatterns.UnionCaseTest(obj,ty,f1) -> printExpr 10 obj + ".Is" + f1.Name
+        | BasicPatterns.UnionCaseTag(obj,ty) -> printExpr 10 obj + ".Tag" 
+        | BasicPatterns.ObjectExpr(ty,basecall,overrides,iimpls) -> "{ new " + printTy ty + " with ... }"
+        | BasicPatterns.TraitCall(tys,nm,argtys,tinst,args) -> "trait call " + nm + printTupledArgs args
+        | BasicPatterns.Const(obj,ty) -> 
+            match obj with 
+            | :? string  as s -> "\"" + s + "\""
+            | null -> "()"
+            | _ -> string obj
+        | BasicPatterns.Value(v) -> v.CompiledName
+        | BasicPatterns.ValueSet(v,e1) -> quote low (v.CompiledName + " <- " + printExpr 0 e1)
+        | BasicPatterns.WhileLoop(e1,e2) -> "while " + printExpr 0 e1 + " do " + printExpr 0 e2 + " done"
+        | _ -> failwith (sprintf "unrecognized %+A" e)
+
+    and quote low s = if low > 0 then "(" + s + ")" else s
+    and printObjOpt e = match e with None -> "" | Some e -> printExpr 10 e + "."
+    and printTupledArgs args = "(" + String.concat "," (List.map (printExpr 0) args) + ")"
+    and printCurriedArgs args = String.concat " " (List.map (printExpr 10) args)
+    and printParams (vs: FSharpMemberOrFunctionOrValue list) = "(" + String.concat "," (vs |> List.map (fun v -> v.CompiledName)) + ")"
+    and printCurriedParams (vs: FSharpMemberOrFunctionOrValue list list) = String.concat " " (List.map printParams vs)
+    and printTy ty = ty.Format(FSharpDisplayContext.Empty)
+    and printTyargs tyargs = match tyargs with [] -> "" | args -> "<" + String.concat "," (List.map printTy tyargs) + ">"
+
+
+    let rec printDeclaration (excludes:HashSet<_> option) (d: FSharpImplementationFileDeclaration) = 
+        seq {
+           match d with 
+            | FSharpImplementationFileDeclaration.Entity(e,ds) ->
+                yield sprintf "type %s" e.LogicalName
+                yield! printDeclarations excludes ds
+            | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v,vs,e) ->
+            
+               if not v.IsCompilerGenerated && 
+                  not (match excludes with None -> false | Some t -> t.Contains v.CompiledName) then
+                let text = 
+                    printfn "%s" v.CompiledName
+                 //try
+                    if v.IsMember then 
+                        sprintf "member %s%s = %s @ %s" v.CompiledName (printCurriedParams vs)  (printExpr 0 e) (e.Range.ToShortString())
+                    else 
+                        sprintf "let %s%s = %s @ %s" v.CompiledName (printCurriedParams vs) (printExpr 0 e) (e.Range.ToShortString())
+                 //with e -> 
+                 //    printfn "FAILURE STACK: %A" e
+                 //    sprintf "!!!!!!!!!! FAILED on %s @ %s, message: %s" v.CompiledName (v.DeclarationLocation.ToString()) e.Message
+                yield text
+            | FSharpImplementationFileDeclaration.InitAction(e) ->
+                yield sprintf "do %s" (printExpr 0 e) }
+    and printDeclarations excludes ds = 
+        seq { for d in ds do 
+                yield! printDeclaration excludes d }
+
+    let rec exprsOfDecl (d: FSharpImplementationFileDeclaration) = 
+        seq {
+           match d with 
+            | FSharpImplementationFileDeclaration.Entity(e,ds) ->
+                yield! exprsOfDecls ds
+            | FSharpImplementationFileDeclaration.MemberOrFunctionOrValue(v,vs,e) ->
+               if not v.IsCompilerGenerated then
+                  yield e, e.Range
+            | FSharpImplementationFileDeclaration.InitAction(e) ->
+                yield e, e.Range }
+    and exprsOfDecls ds = 
+        seq { for d in ds do 
+                yield! exprsOfDecl d }
+
+
+[<EntryPoint>]
+let main argv = 
+    let res =
+        checker.GetProjectOptionsFromProjectFile @"..\..\..\TestProject\TestProject.fsproj"
+        |> checker.ParseAndCheckProject 
+        |> Async.RunSynchronously
+                                                                                       
+    res.AssemblyContents.ImplementationFiles
+    |> Seq.iter (fun f -> f.Declarations |> Seq.iter (printDeclaration None >> Seq.iter (printfn "%s")))
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
